ce9442e63b8bf58ba2a1e674300c8db74f027599
SandroRolak <sandro.rolak2090@gmail.com>
2021-06-25 21:18:24 -0300

    Aninhamentos e Condições Múltiplas

---
+++ Curso em Vídeo/class12/ex12_1.js
@@ -0,0 +1,14 @@
+let agora = new Date()
+let hora = agora.getHours()
+
+console.log(`Agora são exatamente ${hora}!`)
+
+if (hora<7) {
+    console.log("Boa Madrugada!")
+} else if (hora<12) {
+    console.log("Bom Dia!")
+}else if(hora<18) {
+    console.log("Boa Tarde!")
+} else {
+    console.log('Boa Noite!')
+}
