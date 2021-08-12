eae690256daac8476c36f8e479c09519be2c9b43
SandroRolak <sandro.rolak2090@gmail.com>
2021-08-12 16:23:01 -0300

    New test

--- GRASSHOPPER/test.js
+++ GRASSHOPPER/test.js
@@ -11,3 +11,19 @@ var greeting = 'Hello Grasshopper!';
 greeting = greeting.replace('hopper', 'jumper');
 console.log(greeting);
 
+var x = 5;
+if (x>10) {
+    console.log('x is greater than 10');
+} else {
+    console.log('x is less than 10');
+}
+console.log(x);
+
+//Cambista
+function inYen(dollars){
+    let amount = dollars * 113;
+    return amount;
+};
+
+console.log('20 US dollars is: ' + inYen(20) + ' Japanese yen');
+console.log('100 US dollars is: ' + inYen(100) + ' Japanese yes');
