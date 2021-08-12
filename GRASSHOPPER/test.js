08126ee9ee7664ce45aa9b07f6a2a1fc6d592d09
SandroRolak <sandro.rolak2090@gmail.com>
2021-08-12 14:56:32 -0300

    Updates

---
+++ GRASSHOPPER/test.js
@@ -0,0 +1,13 @@
+//Utilizando replace().
+//Example 01
+var message = 'Today we are going shopping';
+
+message = message.replace('day', 'morrow');
+
+console.log(message);
+
+//Example 02
+var greeting = 'Hello Grasshopper!';
+greeting = greeting.replace('hopper', 'jumper');
+console.log(greeting);
+
